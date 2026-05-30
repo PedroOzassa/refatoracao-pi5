add035730f107ece56e35bec24741c676f312f38
PedroOzassa <162368884+PedroOzassa@users.noreply.github.com>
2026-05-29 21:14:49 -0300

    Add context validation and response

--- llm/chat_service.py
+++ llm/chat_service.py
@@ -2,6 +2,12 @@ from llm.mistral_service import chamar_mistral
 from llm.prompt_builder import montar_prompt
 
 def responder(pergunta: str, contexto: str) -> str:
-    prompt = montar_prompt(pergunta, contexto)
-    resposta = chamar_mistral(prompt)
+    if not contexto or contexto.strip() == "":
+        resposta = "Desculpe, não encontrei informações suficientes para responder" \
+        " à sua pergunta. Por favor, reformule sua solicitação ou entre em" \
+        " contato com nossa central de atendimento para obter suporte especializado."
+    else:
+        prompt = montar_prompt(pergunta, contexto)
+        resposta = chamar_mistral(prompt)
+        
     return resposta
